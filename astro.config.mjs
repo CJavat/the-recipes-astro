4810d8e1ba218b923e10096e93e9528977498867
CJavat <cdpm98@hotmail.com>
2024-10-13 23:35:14 -0600

    Estructura del proyecto creado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), vue()],
+
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  experimental: {
+    serverIslands: true,
+  },
+});
